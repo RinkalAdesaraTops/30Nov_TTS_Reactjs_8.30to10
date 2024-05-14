baad5d10455af856b639e91bfb652a265002b245
RinkaladesaraTops <rinkaladesara.tops@gmail.com>
2024-05-14 09:44:27 +0530

    first project & first component created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstComponent from './Components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      My First React Project
+      <FirstComponent />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+// function FirstComponent(){
+
+// }
+const FirstComponent = ()=>{
+    return(
+        <div>
+            <h3>My First Component Created</h3>
+        </div>
+    )
+}
+
+export default FirstComponent;
